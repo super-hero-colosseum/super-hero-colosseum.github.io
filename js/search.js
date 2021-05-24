977a1a32f0d8f89cd0f23e9f551ca9ce9d1b7315
Dom26 <domniniquemoore12@yahoo.com>
2021-05-23 19:46:44 -0700

    Edited search.html, and Added search.js

---
+++ js/search.js
@@ -0,0 +1,8 @@
+// Search file
+// looks through the api to find a selected hero
+
+// API url
+// Contains all the characters in the api
+let url = "https://akabab.github.io/superhero-api/api/all.json";
+// Have to do "all.json" because the api doesn't have an option to
+// search for characters by name
