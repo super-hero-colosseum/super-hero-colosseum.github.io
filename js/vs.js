cf2136ec8efbcd4fc6d43da44aa7f017323c5eac
Darika.B <darikadb@gmail.com>
2021-05-23 20:42:03 -0700

    added not found error handling method in search

--- js/vs.js
+++ js/vs.js
@@ -1,5 +1,19 @@
 let backgroundColors = [
-    ''
+    'rgba(255, 85, 94, 0.8)',
+    'rgba(255, 134, 80, 0.8)',
+    'rgba(255, 233, 129, 0.8)',
+    'rgba(139, 241, 139, 0.8)',
+    'rgba(131, 178, 255, 0.8)',
+    'rgba(155, 110, 243, 0.8)'
+];
+
+let borderColors = [
+    'rgba(255, 85, 94, 1)',
+    'rgba(255, 134, 80, 1)',
+    'rgba(255, 233, 129, 1)',
+    'rgba(139, 241, 139, 1)',
+    'rgba(131, 178, 255, 1)',
+    'rgba(155, 110, 243, 1)'
 ];
 
 let url = "https://akabab.github.io/superhero-api/api/all.json";
@@ -13,60 +27,119 @@ form.addEventListener('submit', (e) => {
     getApiJson();
 })
 
+// function findIfExists(data) {
+//     data.every(element => {
+//         if(heroOneName.value === element.name)
+//             return true;
+//     })
+// }
+
+function isExists(data) {
+    
+
+    if(found1 === true && found2 === true) {
+
+    }
+}
+
+
 function findNames(data) {
     const heroOneInfo = document.getElementById('heroOneInfo');
     const heroTwoInfo = document.getElementById('heroTwoInfo');
 
+    let found1 = false;
+    let found2 = false;
+
     data.forEach(element => {
         if(heroOneName.value === element.name) {
-            let name = document.createElement('h5');
-            heroOneInfo.appendChild(name);
-            name.innerText = element.name;
-            let img = document.createElement('img');
-            img.src = element.images.sm;
-            img.alt = element.name;
-            heroOneInfo.appendChild(img);
-            let realName = document.createElement('p');
-            heroOneInfo.appendChild(realName);
-            if(element.biography.fullName === '') {
-                realName.innerText = 'REAL NAME: same'
-            } else {
-                realName.innerText = 'REAL NAME: ' + element.biography.fullName;
-            }
-            let publisher = document.createElement('p');
-            heroOneInfo.appendChild(publisher);
-            publisher.innerText = 'PUBLISHER: ' + element.biography.publisher;
-            let alignment = document.createElement('p');
-            heroOneInfo.appendChild(alignment);
-            alignment.innerText = 'ALIGNMENT: ' + element.biography.alignment;
+            found1 = true;
         }
 
         if(heroTwoName.value === element.name) {
-            let name = document.createElement('h5');
-            heroTwoInfo.appendChild(name);
-            name.innerText = element.name;
-            let img = document.createElement('img');
-            img.src = element.images.sm;
-            img.alt = element.name;
-            heroTwoInfo.appendChild(img);
-            let realName = document.createElement('p');
-            heroTwoInfo.appendChild(realName);
-            if(element.biography.fullName === '') {
-                realName.innerText = 'REAL NAME: same'
-            } else {
-                realName.innerText = 'REAL NAME: ' + element.biography.fullName;
-            }
-            let publisher = document.createElement('p');
-            heroTwoInfo.appendChild(publisher);
-            publisher.innerText = 'PUBLISHER: ' + element.biography.publisher;
-            let alignment = document.createElement('p');
-            heroTwoInfo.appendChild(alignment);
-            alignment.innerText = 'ALIGNMENT: ' + element.biography.alignment;
-
+            found2 = true;
         }
     })
 
-    //console.log(heroOneName.value);
+    // let found = findIfExists(data);
+    // console.log(found);
+
+
+    if(found1 === true &&  found2 === true) {
+        data.forEach(element => {
+            if(heroOneName.value === element.name) {
+                let name = document.createElement('h5');
+                heroOneInfo.appendChild(name);
+                name.innerText = element.name;
+                let img = document.createElement('img');
+                img.src = element.images.sm;
+                img.alt = element.name;
+                heroOneInfo.appendChild(img);
+                let realName = document.createElement('p');
+                heroOneInfo.appendChild(realName);
+                if(element.biography.fullName === '') {
+                    realName.innerText = 'REAL NAME: same'
+                } else {
+                    realName.innerText = 'REAL NAME: ' + element.biography.fullName;
+                }
+                let publisher = document.createElement('p');
+                heroOneInfo.appendChild(publisher);
+                publisher.innerText = 'PUBLISHER: ' + element.biography.publisher;
+                let alignment = document.createElement('p');
+                heroOneInfo.appendChild(alignment);
+                alignment.innerText = 'ALIGNMENT: ' + element.biography.alignment;
+                
+                //create array for chart to use as data
+                let stats1 = element.powerstats;
+            }
+    
+            if(heroTwoName.value === element.name) {
+                let name = document.createElement('h5');
+                heroTwoInfo.appendChild(name);
+                name.innerText = element.name;
+                let img = document.createElement('img');
+                img.src = element.images.sm;
+                img.alt = element.name;
+                heroTwoInfo.appendChild(img);
+                let realName = document.createElement('p');
+                heroTwoInfo.appendChild(realName);
+                if(element.biography.fullName === '') {
+                    realName.innerText = 'REAL NAME: same'
+                } else {
+                    realName.innerText = 'REAL NAME: ' + element.biography.fullName;
+                }
+                let publisher = document.createElement('p');
+                heroTwoInfo.appendChild(publisher);
+                publisher.innerText = 'PUBLISHER: ' + element.biography.publisher;
+                let alignment = document.createElement('p');
+                heroTwoInfo.appendChild(alignment);
+                alignment.innerText = 'ALIGNMENT: ' + element.biography.alignment;
+                //create array for chart to use as data
+                let stats2 = element.powerstats;
+            } 
+        })
+    }
+
+    if(found1 === true &&  found2 === false) {
+        let error = document.createElement('p');
+        error.setAttribute('color', 'red');
+        heroOneInfo.appendChild(error);
+        error.innerText = 'Second input is not found. Please, try again.';
+    }
+
+    if(found1 === false && found2 === true) {
+        let error = document.createElement('p');
+        error.setAttribute('color', 'red');
+        heroOneInfo.appendChild(error);
+        error.innerText = 'First input is not found. Please, try again.';
+    }
+    
+    if(found1 === false && found2 === false) {
+        let error = document.createElement('p');
+        error.setAttribute('color', 'red');
+        heroOneInfo.appendChild(error);
+        error.innerText = 'Names not found. Please, try again.';
+    }
+
 }
 
 
